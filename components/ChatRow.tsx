1660448a3d55fca7f0ce0973b4c9d0f88b7dc82a
Huzaifa <huzaifaahxan6@gmail.com>
2023-04-30 06:26:38 -0800

    checking responsive layout

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -36,13 +36,13 @@ function ChatRow({ id }: Props) {
       href={`/chat/${id}`}
       className={`chatRow ${active && "bg-gray-700/50"}`}
     >
-      <ChatBubbleLeftIcon className="h-5 w-5" />
+      <ChatBubbleLeftIcon className="w-12 h-6 xs:h-5 xs:w-5 " />
       <p className="flex-1 hidden md:inline-flex truncate">
         {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
       </p>
       <TrashIcon
         onClick={deleteRow}
-        className="h-5 w-5 text-gray-700 hover:text-red-700"
+        className=" w-12 h-6 xs:h-5 xs:w-5 text-gray-700 hover:text-red-700 "
       />
     </Link>
   );
--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -23,7 +23,7 @@ const NewChat = () => {
   };
   return (
     <div onClick={createNewChat} className="border-gray-700 border chatRow">
-      <PlusIcon className="h-4 w-4" />
+      <PlusIcon className="h-4 w-4 " />
       <p>New Chat</p>
     </div>
   );
