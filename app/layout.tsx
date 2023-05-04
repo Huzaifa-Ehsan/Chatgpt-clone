b7dc12b6cb3401a3ebefb04a084acfb441a8d1c5
Huzaifa <huzaifaahxan6@gmail.com>
2023-05-04 11:34:36 -0800

    JWT expired trying to fixing this

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ type Props = {
 };
 export default async function RootLayout({ children }: Props) {
   const session = await getServerSession(authOptions);
-
   return (
     <html lang="en">
       <body>
